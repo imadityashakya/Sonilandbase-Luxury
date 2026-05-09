7fd76512c57f4d9ab614974530bea7ff4e9da61c
AdityaShakya23 <aditya.s@webclusion.com>
2026-05-09 09:03:22 +0530

    Live

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVitePlugin } from '@tanstack/router-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVitePlugin(),
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
